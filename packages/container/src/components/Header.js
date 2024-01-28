37a0f343d5731e87d1c34a5171def15ef5498b16
finz@dhigroup.com <nuzulaz10@gmail.com>
2024-01-28 07:10:23 +0700

    auth yaml

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            Microfrontend App
           </Typography>
           <Button
             color="primary"
